a11ab01ca66188eafb092158b8c4982b2b5e6a45
jroscoe5 <36338176+jroscoe5@users.noreply.github.com>
2018-05-29 16:44:23 -0700

    review POST Route operational

--- expressServer/model/UserModel.js
+++ expressServer/model/UserModel.js
@@ -53,8 +53,14 @@ var UserModel = /** @class */ (function () {
     };
     // add a review to a users collection
     UserModel.prototype.bindReviewToUser = function (userId, reviewId) {
-        var id = Mongoose.Types.ObjectId(userId);
-        this.model.updateOne({ _id: id }, { "$push": { reviews: { reviewID: reviewId.valueOf() } } });
+        console.log(reviewId);
+        var id = new Mongoose.Types.ObjectId(userId);
+        console.log(id);
+        var idVal = reviewId.valueOf();
+        this.model.findOne({ _id: id }, function (err, doc) {
+            doc.reviews.push({ reviewID: idVal });
+            doc.save();
+        });
     };
     return UserModel;
 }());
